1573e77415871f6486585a51f0a6ecbc9a851148
Jake Klassen <jklassendev@gmail.com>
2016-02-07 04:46:46 +0000

    tweaking app.js

--- app.js
+++ app.js
@@ -2,35 +2,36 @@
 
 // URL shortener service
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-// In development, load our .env file.
-// In production (heroku) we'll use the heroku env variables.
-if (process.env.NODE_ENV === 'development') {
-	require('dotenv').config();
-}
-
 const MongoClient = require('mongodb').MongoClient;
 const co = require('co');
 const express = require('express');
 const app = express();
-const routes = require('./routes');
-const port = process.env.PORT || 3000;
 
-app.set('MONGO_HOST', process.env.MONGO_HOST);
-app.set('MONGO_DB', process.env.MONGO_DB);
-app.set('MONGO_COLLECTION', process.env.MONGO_COLLECTION);
+app.set('env', process.env.NODE_ENV || 'development');
+app.set('port', process.env.PORT || 3000);
+
+// In development, load our .env file.
+// In production (heroku) we'll use the heroku env variables.
+if (app.get('env') === 'development') {
+	// Load .env
+	require('dotenv').config();
+
+	app.set('MONGO_HOST', process.env.MONGO_HOST);
+	app.set('MONGO_DB', process.env.MONGO_DB);
+	app.set('MONGO_COLLECTION', process.env.MONGO_COLLECTION);
+}
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 // Setup app
 co(function* run() {
+	const routes = require('./routes');
 	let mongoUrl;
 
-	if (process.env.NODE_ENV === 'development') {
+	if (app.get('env') === 'development') {
 		mongoUrl = `mongodb://${app.get('MONGO_HOST')}/${app.get('MONGO_DB')}`;
-	} else if (process.env.NODE_ENV === 'production') {
+	} else if (app.get('env') === 'production') {
 		// Heroku variable
 		mongoUrl = process.env.MONGOLAB_URI;
 	}
@@ -50,7 +51,7 @@ co(function* run() {
 	routes.db(app, collection);
 	routes.url(app, collection);
 
-	app.listen(port, () => {
-		console.log(`Server listening on port ${port}`);
+	app.listen(app.get('port'), () => {
+		console.log(`Server listening on port ${app.get('port')}`);
 	});
 });
